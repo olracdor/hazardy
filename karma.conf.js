39fa5ab2b98d712f7c934b67fa491440dfaa9e7c
Rod Santillan <rod.santillan@z.co.nz>
2020-07-15 20:53:08 +1200

    fixed build

--- karma.conf.js
+++ karma.conf.js
@@ -7,8 +7,8 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
+      require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require( 'karma-phantomjs-launcher' ),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
@@ -25,7 +25,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['PhantomJS'],
+    browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true
   });
